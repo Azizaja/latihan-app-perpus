fd603b5dd176fe0501b8c14dd76720efe5ac2b60
Azizaja <azizsetiawan@students.amikom.ac.id>
2023-07-06 16:59:09 +0700

    add slider

--- latihan-app/resources/views/layouts/app.blade.php
+++ latihan-app/resources/views/layouts/app.blade.php
@@ -31,9 +31,14 @@
     <!-- owl stylesheets -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" rel="stylesheet" />
     <!-- jquery -->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jqueryui.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <!-- <script src="formProgressBar.jquery.js"></script> -->
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+ 
+
 </head>
 
 <body>
--- latihan-app/resources/views/rak_buku/form.blade.php
+++ latihan-app/resources/views/rak_buku/form.blade.php
@@ -1,11 +1,69 @@
 @extends('layouts.app')
 @section('title', 'Daftar Rak Buku')
 @section('content')
+<style>
+    /* #progressbar{
+        position:relative;
+	width:100%;
+	height:22px;
+	background:rgba(255,255,255,.05);
+	border-radius:25px;
+	display:flex;
+	align-items:center;
+	border:5px solid #1b1f23;
+    } */
+    /* .container{
+	position:relative;
+	text-align:center;
+}
+.container .Loading{
+	position:relative;
+	width:100%;
+	height:22px;
+	background:rgba(255,255,255,.05);
+	border-radius:25px;
+	display:flex;
+	align-items:center;
+	border:1px solid #1b1f23;
+}
+.container .Loading span{
+	position:absolute;
+	width:0%;
+	height:100%;
+	background-color:#0fbcf9;
+	border-radius:25px;
+}
+.container i{
+	color:#fff;
+	font-size:5em;
+} */
+</style>
 <script>
         $(function () {
             $("#datepicker").datepicker();   
         });
-    </script>
+        $( function() {
+    $( document ).tooltip();
+  } );
+</script>
+<script>
+  $( function() {
+    $( "#slider-range-max" ).slider({
+      range: "max",
+      min: 1,
+      max: 100,
+      value: 0,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
+  } );
+  </script>
+
+
+<!-- <div id="progressbar"></div> -->
+
     <h2>{{ $store }} Data Rak Buku</h2>
     <form method="POST" action="{{$action}}">
         @csrf
@@ -13,7 +71,7 @@
             @method('PUT')
         @endif
         <input type="hidden" name="id" value="{{ $rak->id }}" />
-        <input type="text" class="mail_text" name="nama" placeholder="Nama Rak" value="{{ $rak->nama }}" /><br>
+        <input type="text" class="mail_text" name="nama" placeholder="Nama Rak" value="{{ $rak->nama }}" title="Masukkan Nama Rak"/><br>
         @error('nama')
             <b>{{ $message }}</b>
         @enderror<br>
@@ -22,11 +80,16 @@
             <b>{{ $message }}</b>
         @enderror<br>
         <input type="text" class="mail_text" name="keterangan" placeholder="keterangan" value="{{ $rak->keterangan }}" /><br>
-        <input type="text"  class="mail_text" id="datepicker" name="tanggal" placeholder="tanggal" title="Masukkan tanggal"/></br>
+        <input type="text"  class="mail_text" id="datepicker" name="tanggal" placeholder="tanggal" /></br>
+        
         <input type="submit" class="btn btn-success mt-3" value="{{ $store }}" />
         <div class="send_bt">
             <a href="{{ url('/rak_buku') }}">Kembali</a>
         </div>
     </form>
-   
+        <p>
+        <label for="amount">jumlah minimal:</label>
+        <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
+        </p>
+        <div id="slider-range-max"></div><br>
 @endsection
